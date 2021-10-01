057cb9e9dde4dd409a61e8f3e0c12e063a1af5c5
regola-Tacens <regolas-tacens@outlook.fr>
2021-10-01 12:35:07 +0200

    ajout de l'option ssl

--- app/database.js
+++ app/database.js
@@ -1,11 +1,26 @@
 const {Sequelize} = require('sequelize');
 
-
-
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     define: {
         underscored: true,
-    }
+    },
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
+
 module.exports = sequelize;
+
+////////////////////const Pool = require("pg").Pool;
+// const Pool = require('pg').Pool;
+
+// require('dotenv').config();
+
+// const sequelize = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//         rejectUnauthorized: false,
+//     },
+// });
+// module.exports = sequelize;
