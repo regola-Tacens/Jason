d1aee2dc759611a4bd6343aba1c03aded7115063
regola-Tacens <regolas-tacens@outlook.fr>
2021-10-01 15:38:05 +0200

    ssl required true

--- app/database.js
+++ app/database.js
@@ -7,7 +7,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
-        ssl: true
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
     }
 });
 
