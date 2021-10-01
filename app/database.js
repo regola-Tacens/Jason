bc51d2f39c8430ef6c985eefb34dba5a52c4a700
regola-Tacens <regolas-tacens@outlook.fr>
2021-10-01 15:24:21 +0200

    ajout de config.json

--- app/database.js
+++ app/database.js
@@ -12,6 +12,11 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 
 module.exports = sequelize;
 
+// const sequelize = new Sequelize('argo', 'argo', 'argo', {
+//   host: 'localhost',
+//   dialect: 'postgres'
+// });
+
 ////////////////////const Pool = require("pg").Pool;
 // const Pool = require('pg').Pool;
 
