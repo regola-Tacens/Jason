f2ca103ab202489e8d603188c0c767335c5930f2
regola-Tacens <regolas-tacens@outlook.fr>
2021-10-02 07:20:48 +0200

    ajout option ssl heroku

--- app/database.js
+++ app/database.js
@@ -6,13 +6,13 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     },
     dialect: 'postgres',
     protocol: 'postgres',
-    // pour la prod decommenter dialect Options
-    // dialectOptions: {
-    //     ssl: {
-    //         require: true,
-    //         rejectUnauthorized: false
-    //     }
-    // }
+    pour la prod decommenter dialect Options
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 module.exports = sequelize;
