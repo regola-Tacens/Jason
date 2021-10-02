5a8af342ac025bf80193d45827e98cd0186a5f8e
regola-Tacens <regolas-tacens@outlook.fr>
2021-10-02 07:23:55 +0200

    commentaire en trop sur ssl

--- app/database.js
+++ app/database.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     },
     dialect: 'postgres',
     protocol: 'postgres',
-    pour la prod decommenter dialect Options
+    // pour la prod decommenter dialect Options
     dialectOptions: {
         ssl: {
             require: true,
